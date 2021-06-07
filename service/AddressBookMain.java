6186facf265e8412acce3ba97e77b7b1ae749b5d
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-07 23:59:17 +0530

    added: Edit details in address book

--- service/AddressBookMain.java
+++ service/AddressBookMain.java
@@ -1,9 +1,11 @@
+
 /**************************************************
  * Purpose : Address Book System
  * @author Rosy Rupali
  * @since 07-06-2021
  *
  *************************************************/
+
 package service;
 
 import java.util.ArrayList;
@@ -11,36 +13,78 @@ import java.util.List;
 import java.util.Scanner;
 import model.Person;
 
+
+
 public class AddressBookMain {
 	private static Scanner scanner = new Scanner(System.in);
 	List<Person> personList = new ArrayList<>();
 	Person person = new Person();
 
+
 	/**
 	 * This method add the person detail in ArrayList
 	 */
-
 	public void add() {
 		System.out.println("Enter your First Name: ");
-		person.setFirstName(scanner.nextLine());
+		person.setFirstName(scanner.next());
 		System.out.println("Enter your Last Name: ");
-		person.setLastName(scanner.nextLine());
+		person.setLastName(scanner.next());
 		System.out.println("Enter your address: ");
-		person.setAddress(scanner.nextLine());
+		person.setAddress(scanner.next());
 		System.out.println("Enter your City: ");
-		person.setCity(scanner.nextLine());
+		person.setCity(scanner.next());
 		System.out.println("Enter your State: ");
-		person.setState(scanner.nextLine());
+		person.setState(scanner.next());
 		System.out.println("Enter your Zip: ");
 		person.setZip(scanner.nextInt());
 		System.out.println("Enter your Phone Number: ");
 		person.setPhoneNumber(scanner.nextInt());
 		System.out.println("Enter your Email: ");
-		person.setEmail(scanner.nextLine());
+		person.setEmail(scanner.next());
 
 		personList.add(person);
 		System.out.println(personList);
 
+	}
+	
+	/**
+	 * This method is used to edit the details in contact based on the entered name
+	 */
+	public void edit() {
+		String name=null;
+		System.out.println("Enter the name of the record which we want to Edit");
+		name = scanner.next();
+		if(personList.isEmpty())
+		{
+			System.out.println("No records to edit");
+			return;
+		}
+		
+			for(int i=0;i<personList.size();i++)
+			{
+				if((personList.get(i).getFirstName().equals(name)) || (personList.get(i).getLastName().equals(name)))
+				{
+					System.out.println("Enter First name:");
+					person.setFirstName(scanner.next());
+					System.out.println("Enter Last name:");
+					person.setLastName(scanner.next());
+					System.out.println("Enter your address:");
+					person.setAddress(scanner.next());
+					System.out.println("Enter your city:");
+					person.setCity(scanner.next());
+					System.out.println("Enter your state:");
+					person.setState(scanner.next());
+					System.out.println("Enter your ZIP code:");
+					person.setZip(scanner.nextInt());
+					System.out.println("Enter your phone number");
+					person.setPhoneNumber(scanner.nextInt());
+					
+					personList.remove(i);
+					personList.add(i, person);
+				}
+				
+			}
+		
 	}
 
 	public static void main(String[] args) {
@@ -56,6 +100,9 @@ public class AddressBookMain {
 			switch (option) {
 			case 1:
 				book.add();
+				break;
+			case 2:
+				book.edit();
 				break;
 			default:
 				System.out.println("Invalid option");
